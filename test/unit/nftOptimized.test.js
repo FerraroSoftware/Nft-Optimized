2ab1a1f75e949ee338ac38316c9b56a83ac67718
FerraroSoftware <software.ferraro@gmail.com>
2022-06-23 18:54:08 -0400

    compile test

--- test/unit/nftOptimized.test.js
+++ test/unit/nftOptimized.test.js
@@ -5,14 +5,11 @@ const { developmentChains } = require("../../helper-hardhat-config");
 !developmentChains.includes(network.name)
   ? describe.skip
   : describe("Optimized NFT Unit Tests", function () {
-      let basicNft, deployer;
+      let nftOptimized, deployer;
 
       beforeEach(async () => {
         accounts = await ethers.getSigners();
         deployer = accounts[0];
-        //   await deployments.fixture(["mocks", "basicnft"]);
-        basicNft = await ethers.getContract("NftOptimized");
+        nftOptimized = await ethers.getContract("NftOptimized");
       });
-
-      it("Allows users to mint an NFT, and updates appropriately", async function () {});
     });
